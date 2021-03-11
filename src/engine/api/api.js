6510a93069ab5b0b2350ee22b99d8db55fe168c3
Raffaele Morí <mori.raffaele@gmail.com>
2021-03-11 10:33:46 +0000

    introduced new API requests.

--- src/engine/api/api.js
+++ src/engine/api/api.js
@@ -1,7 +1,24 @@
-function getInfo({page = 1} = {}) {
-    return fetch(`https://swapi.dev/api/people/?format=json&page=${page}`);
+const SERVER_URL = 'https://swapi.dev/api';
+
+function getCharactersInfo({page = 1} = {}) {
+    return getInfo('people',  page);
+}
+
+function getPlanetsInfo({page = 1} = {}) {
+    return getInfo('planets',  page);
+}
+
+function getFilmsInfo({page = 1} = {}) {
+    return getInfo('films',  page);
+}
+
+function getInfo(infoType, page) {
+    return fetch(`${SERVER_URL}/${infoType}/?page=${page}`)
+        .then(r => r.json());
 }
 
 export default {
-    getInfo
+    getCharactersInfo,
+    getPlanetsInfo,
+    getFilmsInfo
 };
--- src/engine/api/api.test.js
+++ src/engine/api/api.test.js
@@ -6,16 +6,57 @@ describe('api', () => {
         jest.spyOn(global, 'fetch').mockResolvedValue({
             json: () => fetchResponse
         });
-    })
+    });
     afterEach(() => {
         global.fetch.mockClear();
     });
-    it('should call fetch twice', () => {
-        api.getInfo();
-        expect(global.fetch).toBeCalledWith('https://swapi.dev/api/people/?format=json&page=1');
+
+    describe('getCharactersInfo method' , () => {
+        it('should call fetch once with correct url', () => {
+            api.getCharactersInfo();
+            expect(global.fetch).toBeCalledWith('https://swapi.dev/api/people/?page=1');
+        });
+    
+        it('should call fetch once with correct page', () => {
+            api.getCharactersInfo({page: 3});
+            expect(global.fetch).toBeCalledWith('https://swapi.dev/api/people/?page=3');
+        });
+    
+        it('should resolve with the 2 arrays concatenated', () => {
+            expect(api.getCharactersInfo()).resolves.toEqual(fetchResponse);
+        });
     });
 
-    it('should resolve with the 2 arrays concatenated', () => {
-        expect(api.getInfo()).resolves.toEqual(fetchResponse);
+    describe('getPlanetsInfo method' , () => {
+        it('should call fetch once with correct url', () => {
+            api.getPlanetsInfo();
+            expect(global.fetch).toBeCalledWith('https://swapi.dev/api/planets/?page=1');
+        });
+    
+        it('should call fetch once with correct page', () => {
+            api.getPlanetsInfo({page: 3});
+            expect(global.fetch).toBeCalledWith('https://swapi.dev/api/planets/?page=3');
+        });
+    
+        it('should resolve with the 2 arrays concatenated', () => {
+            expect(api.getPlanetsInfo()).resolves.toEqual(fetchResponse);
+        });
     });
+
+    describe('getFilmsInfo method' , () => {
+        it('should call fetch once with correct url', () => {
+            api.getFilmsInfo();
+            expect(global.fetch).toBeCalledWith('https://swapi.dev/api/films/?page=1');
+        });
+    
+        it('should call fetch once with correct page', () => {
+            api.getFilmsInfo({page: 3});
+            expect(global.fetch).toBeCalledWith('https://swapi.dev/api/films/?page=3');
+        });
+    
+        it('should resolve with the 2 arrays concatenated', () => {
+            expect(api.getFilmsInfo()).resolves.toEqual(fetchResponse);
+        });
+    });
+    
 });
